8db68f9bd4f61aa9391b3629c3a16b4071e76cbb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-09 16:41:08 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -80,22 +80,6 @@ then
     /bin/chmod 755 ${HOME}/runtime
 fi
 
-#Load the parts of the configuration that we need into memory
-GIT_EMAIL_ADDRESS="`${HOME}/utilities/config/ExtractConfigValue.sh 'GITEMAILADDRESS'`"
-BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
-ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^ //g' | /bin/sed 's/ /./g'`"
-GIT_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'GITUSER'  | /bin/sed 's/#/ /g'` "
-
-#Initialise Git
-/usr/bin/git config --global user.name "${GIT_USER}"
-/usr/bin/git config --global user.email ${GIT_EMAIL_ADDRESS}
-/usr/bin/git config --global init.defaultBranch main
-/usr/bin/git config --global pull.rebase false 
-
-
-/bin/echo "${0} Setting up firewall"
-${HOME}/security/SetupFirewall.sh
-
 /bin/echo "${0} Initialising cloudhost config"
 ${HOME}/providerscripts/cloudhost/InitialiseCloudhostConfig.sh
 
@@ -107,8 +91,3 @@ ${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
 /bin/echo "${0} Initialising crontab"
 ${HOME}/cron/InitialiseCron.sh
 
-cd /home
-while ( [ `/bin/ls -1 X*X | wc -l` -gt 1 ] )
-do
-	/bin/rm -r `/bin/ls -cr1 X*X | /usr/bin/head -1`
-done

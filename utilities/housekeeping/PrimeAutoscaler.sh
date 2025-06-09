c72b2db82c6d8e84b51e40fba2ff31951c456b11
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-09 20:57:04 +0100

    Create PrimeAutoscaler.sh

---
+++ utilities/housekeeping/PrimeAutoscaler.sh
@@ -0,0 +1,10 @@
+HOME="`/bin/cat /home/homedir.dat`"
+
+/bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
+
+USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
+
+original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
+
+
+/bin/rm -r /home/${original_user}

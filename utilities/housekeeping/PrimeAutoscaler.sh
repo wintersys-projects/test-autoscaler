d349b8e41fd752a8fd7c29109bae5eb9eca30205
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-11 18:53:25 +0100

    Update PrimeAutoscaler.sh

--- utilities/housekeeping/PrimeAutoscaler.sh
+++ utilities/housekeeping/PrimeAutoscaler.sh
@@ -1,4 +1,5 @@
 HOME="`/bin/cat /home/homedir.dat`"
+BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
@@ -6,5 +7,6 @@ USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
 
 original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
 
-
 /bin/rm -r /home/${original_user}
+
+${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &

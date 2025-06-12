32be4372429e68f7f00a230f695587eb5e800c2d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 21:20:40 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -20,43 +20,7 @@
 #######################################################################################################
 #set -x
 
-#Get ourselves oriented and prepared
-USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
-/bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
-/bin/chmod 644 /home/${USER_HOME}/.bashrc
-/bin/chown ${USER_HOME}:root /home/${USER_HOME}/.bashrc
-
-SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
-
-/bin/echo "set mouse=r" > /root/.vimrc
-
-#Set the intial permissions for the build
-/usr/bin/find ${HOME} -not -path '*/\.*' -type d -print0 | xargs -0 chmod 0755 # for directories
-/usr/bin/find ${HOME} -not -path '*/\.*' -type f -print0 | xargs -0 chmod 0500 # for files
-/bin/chown ${SERVER_USER}:root ${HOME}/.ssh
-/bin/chmod 750 ${HOME}/.ssh
-
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/sh ${1} ${2} ${3} ${4} ${5} ${6}' > /usr/bin/run
-/bin/chown ${SERVER_USER}:root /usr/bin/run
-/bin/chmod 750 /usr/bin/run
-
-if ( [ ! -d ${HOME}/logs/initialbuild ] )
-then
-    /bin/mkdir -p ${HOME}/logs/initialbuild
-fi
-
-if ( [ ! -d ${HOME}/super ] )
-then
-    /bin/mkdir ${HOME}/super
-fi
-
-/bin/mv ${HOME}/utilities/security/Super.sh ${HOME}/super
-/bin/chmod 400 ${HOME}/super/Super.sh
-
-if ( [ -f ${HOME}/InstallGit.sh ] )
-then
-    /bin/rm ${HOME}/InstallGit.sh
-fi
+/bin/rm ${HOME}/logs/*
 
 out_file="initialbuild/autoscaler-build-out-`/bin/date | /bin/sed 's/ //g'`"
 exec 1>>${HOME}/logs/${out_file}
@@ -64,30 +28,4 @@ err_file="initialbuild/autoscaler-build-err-`/bin/date | /bin/sed 's/ //g'`"
 exec 2>>${HOME}/logs/${err_file}
 
 
-/bin/echo "${0} `/bin/date`: Beginning the build of the autoscaler" 
-
-#Create the config directories, these will be mounted from the autoscaler to the other server types - DB, WS and Images Servers
-if ( [ ! -d ${HOME}/.ssh ] )
-then
-    /bin/mkdir ${HOME}/.ssh
-    /bin/chmod 700 ${HOME}/.ssh
-fi
-
-if ( [ ! -d ${HOME}/runtime ] )
-then
-    /bin/mkdir -p ${HOME}/runtime
-    /bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/runtime
-    /bin/chmod 755 ${HOME}/runtime
-fi
-
-#/bin/echo "${0} Initialising cloudhost config"
-#${HOME}/providerscripts/cloudhost/InitialiseCloudhostConfig.sh
-
-cd ${HOME}
-
-#/bin/echo "${0} Initialising datastore config"
-#${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
-
-/bin/echo "${0} Initialising crontab"
-${HOME}/cron/InitialiseCron.sh
 
